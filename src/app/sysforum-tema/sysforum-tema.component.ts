95dccf88c26e32026860b719cb3fce4d08aa327f
Fernando Hernandez <hernandezfernando18982012@gmail.com>
2018-08-09 23:38:16 -0600

    componente Tema creado

---
+++ src/app/sysforum-tema/sysforum-tema.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sysforum-tema',
+  templateUrl: './sysforum-tema.component.html',
+  styleUrls: ['./sysforum-tema.component.css']
+})
+export class SYSFORUMTEMAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
