d9e55c3c3f07afaeedea1f129c1b502f16c7d4cb
Ruben Osorio <darkGunnerArmy@bitbucket.org>
2018-08-20 02:37:34 -0600

    sidebar funcional

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
-
-
+import { SYSFORUMTEMAComponent } from '../../sysforum-tema/sysforum-tema.component';
 export const AdminLayoutRoutes: Routes = [
 
 ];
