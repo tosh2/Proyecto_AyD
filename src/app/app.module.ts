b16b8493b0a48e23cc71f5cc94e89bcc61921718
yoselin-19 <annelice119@gmail.com>
2018-08-20 15:36:57 -0600

    Ordenando algunas cosas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { SYSFORUMTEMAComponent } from './sysforum-tema/sysforum-tema.component';
-
 import {environment} from '../environments/environment';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule, AngularFirestore} from 'angularfire2/firestore';
-
-import {ServicioTemaService} from './sysforum-services/servicio-tema.service';
 import {AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
 
 import {FormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+
+import { SYSFORUMTEMAComponent } from './sysforum-tema/sysforum-tema.component';
+import {ServicioTemaService} from './sysforum-services/servicio-tema.service';
 import { SysforumListaTemasComponent } from './sysforum-lista-temas/sysforum-lista-temas.component';
 import { SysforumVerTemaComponent } from './sysforum-ver-tema/sysforum-ver-tema.component';
-import { AppRoutingModule } from './/app-routing.module';
 import { SysforumPageNotFoundComponent } from './sysforum-page-not-found/sysforum-page-not-found.component';
-
 import {SysforumListarComentariosService} from './sysforum-services/sysforum-listar-comentarios.service';
 
 @NgModule({
