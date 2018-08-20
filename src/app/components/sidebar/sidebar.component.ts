8b6686b913705a0124ce32026a58d41f01dc8708
Ruben Osorio <darkGunnerArmy@bitbucket.org>
2018-08-19 21:32:35 -0600

    actualizacion dash

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
+    { path: '/CrearTema', title: 'Crear Tema', icon: 'design_app', class:'' },
     { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'ui-1_bell-53', class: '' },
