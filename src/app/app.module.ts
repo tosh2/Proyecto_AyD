0ccd584e85ab7ed36bcc0a332116c9093a1354cd
yoselin-19 <annelice119@gmail.com>
2018-08-20 16:03:16 -0600

    Cambios para interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { AppComponent } from './app.component';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { SYSFORUMTEMAComponent } from './sysforum-tema/sysforum-tema.component';
->>>>>>> 44dcd97a7b33f8832f4df44414a970d0dd15f755
 import {environment} from '../environments/environment';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule, AngularFirestore} from 'angularfire2/firestore';
-<<<<<<< HEAD
-=======
-import {ServicioTemaService} from './sysforum-services/servicio-tema.service';
->>>>>>> 44dcd97a7b33f8832f4df44414a970d0dd15f755
 import {AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +22,6 @@ import {SysforumListarComentariosService} from './sysforum-services/sysforum-lis
 
 
 import { ComponentsModule } from './components/components.module';
-import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { ROUTES } from '../sidebar/sidebar.component';
 import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import { Router } from '@angular/router';
-import Chart from 'chart.js';
+//import Chart from 'chart.js';
 
 @Component({
   selector: 'app-navbar',
