fec53acd137a49e97965d49d5157ccecb4f3f991
fernando29hernadez <hernandezfernando18982012@gmail.com>
2018-09-08 22:33:46 -0600

    Prueba de BDD del feature Dashboard

--- e2e/features/dashboard.feature
+++ e2e/features/dashboard.feature
@@ -1,4 +1,7 @@
 Feature: Dashboard
-Scenario: A first scenario
-When yo cargue la pagina en el navegador
-Then ver un menu que se llame "CREAR TEMA"
+Scenario: Carga de los menus del panel principal
+Given entre a la aplicacion
+When en el navegador se carga la pagina principal
+Then ver un primer menu que se llame "CREAR TEMA"
+And ver un segundo menu que se llame "LISTAR TEMAS"
+And ver un tercer menu que se llame "TEMAS"
--- e2e/steps/dashboard.steps.ts
+++ e2e/steps/dashboard.steps.ts
@@ -2,16 +2,24 @@ import { expect } from 'chai';
 import { defineSupportCode } from 'cucumber';
 import { browser, by, element } from 'protractor';
 import { Dashboard } from './dashboard.po';
-defineSupportCode(({ When, Then, Before, And}) => {
+defineSupportCode(({ When, Given , Then, Before, And}) => {
 let home: Dashboard;
     Before(() => {
+    });
+    Given(/^entre a la aplicacion$/, {timeout: 60 * 1000}, () => {
         home = new Dashboard();
     });
-    When(/^yo cargue la pagina en el navegador$/, {timeout: 60 * 1000}, async () => {
+    When(/^en el navegador se carga la pagina principal$/, {timeout: 60 * 1000}, async () => {
         await browser.get('http://localhost:4200');
     });
-    Then(/^ver un menu que se llame "(.*?)"$/,
+    Then(/^ver un primer menu que se llame "(.*?)"$/,
         (string: String) => home.getParagraphText()
             .then(h1title => expect(h1title).to.equal(string)));
+    Then(/^ver un segundo menu que se llame "(.*?)"$/,
+        (string: String) => home.getParagraphText2()
+        .then(h1title => expect(h1title).to.equal(string)));
+    Then(/^ver un tercer menu que se llame "(.*?)"$/,
+        (string: String) => home.getParagraphText3()
+        .then(h1title => expect(h1title).to.equal(string)));
     }
 );
